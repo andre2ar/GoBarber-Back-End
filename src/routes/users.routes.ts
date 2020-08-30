1d7079dc2841d7b03804080bd6631162a17d6057
André Alvim Ribeiro <andre2ar@outlook.com>
2020-08-30 10:26:28 -0300

    Update Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import CreateUserService from "../services/CreateUserService";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 import multer from "multer";
 import uploadConfig from "../config/upload";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -38,9 +39,23 @@ usersRouter.patch(
         request,
         response
     ) => {
-        return response.status(201).json({
-            message: "Olá"
-        });
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename
+            });
+
+            // @ts-ignore
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({
+                error: err.message
+            });
+        }
 });
 
 export default usersRouter;
