3dc483589d7dd968ec74d3bf75e1aeed19cfe043
André Alvim Ribeiro <andre2ar@outlook.com>
2020-10-18 19:54:42 -0300

    Ethereal mail provider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,37 @@
+import nodemailer, {Transporter} from 'nodemailer';
+import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider";
+
+interface IMessage {
+    to: string;
+    body: string;
+}
+
+export default class EtherealMailProvider implements IMailProvider {
+    private client: Transporter;
+    constructor() {
+        const account = nodemailer.createTestAccount().then(account => {
+            this.client = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass
+                }
+            });
+        });
+    }
+
+    public async sendMail(to: string, body: string): Promise<void> {
+        const message = await this.client.sendMail({
+            from: 'GoBarber Team <team@gobarber.com>',
+            to,
+            subject: 'Password recover',
+            text: body,
+        });
+
+        console.log('Message sent: %s', message.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+    }
+}
