714bb709ee5607514c631c6d57d618c4bcc26313
André Alvim Ribeiro <andre2ar@outlook.com>
2020-11-20 17:21:46 -0300

    S3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -10,7 +10,7 @@ import {celebrate, Joi, Segments} from "celebrate";
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post('/', celebrate({
     [Segments.BODY]: {
