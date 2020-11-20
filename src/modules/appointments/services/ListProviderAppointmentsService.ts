be536e15cd7daf10c9b329536c360e3d81832054
André Alvim Ribeiro <andre2ar@outlook.com>
2020-11-20 18:01:22 -0300

    Redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
 import {inject, injectable} from "tsyringe";
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
+import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
 
 interface IRequest {
     provider_id: string;
@@ -13,15 +14,22 @@ interface IRequest {
 export default class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
+        private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ provider_id, day, month, year}: IRequest): Promise<Appointment[]> {
-        return await this.appointmentsRepository.findAllInDayFromProvider({
+        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id,
             day,
             month,
             year
         });
+
+        //await this.cacheProvider.save('teste', 'alksdflak');
+
+        return appointments;
     }
 }
