658e7d68e64b73465bfd82eb1c8714192309c37d
André Alvim Ribeiro <andre2ar@outlook.com>
2021-01-23 19:45:14 -0300

    After outage

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -17,9 +17,9 @@ class AppointmentsRepository implements IAppointmentsRepository {
         });
     }
 
-    public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment> {
+    public async create({provider_id, user_id, date}: ICreateAppointmentDTO): Promise<Appointment> {
         const appointment = this.ormRepository.create({
-            provider_id, date
+            provider_id, user_id, date
         });
 
         await this.ormRepository.save(appointment);
