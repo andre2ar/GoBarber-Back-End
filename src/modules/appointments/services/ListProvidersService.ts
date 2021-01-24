31bef464afa9ca0c0dcfaccd6e8f4c48ee41b5ef
André Alvim Ribeiro <andre2ar@outlook.com>
2021-01-24 16:00:20 -0300

    Profile proper validation

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import User from "@modules/users/infra/typeorm/entities/User";
 import IUsersRepository from "@modules/users/respositories/IUsersRepository";
 import {inject, injectable} from "tsyringe";
 import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
+import {classToClass} from "class-transformer";
 
 @injectable()
 export default class ListProvidersService {
@@ -21,7 +22,10 @@ export default class ListProvidersService {
                 except_user_id: user_id
             });
 
-            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+            await this.cacheProvider.save(
+                `providers-list:${user_id}`,
+                classToClass(users)
+            );
         }
 
         return users;
