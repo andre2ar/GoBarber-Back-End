76f563e9cef13205e46902d8d2a425bc9ae8704b
André Alvim Ribeiro <andre2ar@outlook.com>
2021-01-24 15:47:56 -0300

    Profile proper validation

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -14,8 +14,8 @@ profileRouter.put('/', celebrate({
     [Segments.BODY]: {
         name: Joi.string().required(),
         email: Joi.string().email().required(),
-        old_password: Joi.string(),
-        password: Joi.string(),
+        old_password: Joi.string().min(6).allow(null, ''),
+        password: Joi.string().min(6).allow(null, ''),
         password_confirmation: Joi.string().valid(Joi.ref('password')),
     },
 }), profileController.update);
